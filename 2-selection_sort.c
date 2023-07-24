85673ad6efe85cd3ad331323015ea6b0c546bb5d
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-07-24 19:04:40 +0300

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,51 +1,50 @@
 /*
- * File: 1-insertion_sort_list.c
- * Auth: haru-voster
+ * File: 2-selection_sort.c
+ * Auth: Haru-voster
  */
-
 #include "sort.h"
-/**
- *swapNodes - Swapt two nodes in a doubly linekd list.
- *@list: pointer to the head of the doubly linekd list.
- *@swap1: A pointer to the first node to swap.
- *@swap2: The second node to swap.
- */
-
-void swapNodes(listint_t **list, listint_t **swap1, listint_t *swap2)
-{
-	(*swap1)->next = swap2->next;
-	if (swap2->next != NULL)
-		swap2->next->prev = *swap1;
-	swap2->prev = (*swap1)->prev;
-	swap2->next = *swap1;
-	if ((*swap1)->prev != NULL)
-		(*swap1)->prev->next = swap2;
-	else
-		*list = swap2;
-	(*swap1)->prev = swap2;
-	*swap1 = swap2->prev;
-}
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in
- * ascending order using the insertion sort algorithm.
- * @list: The list to be sorted
+ * selection_sort - Sorts an array of integers in ascedning order
+ *  using the selection sort algorithm.
+ *@array: The array to be sorted
+ *@size: The size of the array
  */
-void insertion_sort_list(listint_t **list)
+void selection_sort(int *array, size_t size)
 {
-	listint_t *a, *insert, *tmp;
+	size_t a, b, jmin, jtmp;
+	int itmp;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
-
-	for (a = (*list)->next; a != NULL; a = tmp)
+	/* loop through the entire array */
+	for (a = 0; a < size - 1; a++)
 	{
-		tmp = a->next;
-		insert = a->prev;
-		while (insert != NULL && a->n < insert->n)
+		/*
+		 * Find the smallest element in the unsorted array[a...size-1]
+		 * Assume the first element is the minimum
+		 */
+		jmin = a;
+		/* Compare against elements after a to find the smallest */
+		for (b = a + 1; b < size; b++)
 		{
-			swapNodes(list, &insert, a);
-			print_list((const listint_t *)*list);
+
+			/* if this element is smaller, then it is the new smallest */
+			if (array[b] < array[jmin])
+			{
+				/* found the new smallest; remember the index */
+				jmin = b;
+			}
+		}
+		/* swap */
+		if (jmin != a)
+		{
+			itmp = array[a]; /*
+					  * Save the value at index a to be swapped
+					  * with the smallest element later
+					  */
+			jtmp = jmin; /* Save the samllest elements index before swapping */
+			array[a] = array[jmin];
+			array[jtmp] = itmp;
+			print_array(array, size);
 		}
 	}
 }
