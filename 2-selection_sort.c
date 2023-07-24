c71f94a775dfb683895195972f813b35d20b2f21
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-07-24 18:44:36 +0300

    selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,7 +1,6 @@
-
 /*
  * File: 2-selection_sort.c
- * Auth: Haru-voster
+ * Auth: haru-voster
  */
 #include "sort.h"
 
@@ -13,38 +12,38 @@
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t x, y, min, ytmp;
-	int xtmp;
+	size_t x, y, jmin, jtmp;
+	int itmp;
 
 	/* loop through the entire array */
 	for (x = 0; x < size - 1; x++)
 	{
 		/*
-		 * Find the smallest element in the unsorted array[x...size-1]
+		 * Find the smallest element in the unsorted array[a...size-1]
 		 * Assume the first element is the minimum
 		 */
-		min = x;
+		jmin = x;
 		/* Compare against elements after a to find the smallest */
-		for (y =  x+ 1;  y< size; y++)
+		for (b = x + 1; y < size; y++)
 		{
 
 			/* if this element is smaller, then it is the new smallest */
-			if (array[y] < array[min])
+			if (array[y] < array[jmin])
 			{
 				/* found the new smallest; remember the index */
-				min = y;
+				jmin = y;
 			}
 		}
 		/* swap */
-		if (min != x)
+		if (jmin != x)
 		{
-			xtmp = array[x]; /*
+			itmp = array[y]; /*
 					  * Save the value at index a to be swapped
 					  * with the smallest element later
 					  */
-			ytmp = min; /* Save the samllest elements index before swapping */
-			array[x] = array[min];
-			array[ytmp] = xtmp;
+			jtmp = jmin; /* Save the samllest elements index before swapping */
+			array[x] = array[jmin];
+			array[jtmp] = itmp;
 			print_array(array, size);
 		}
 	}
