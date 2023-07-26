d279aff9319247fffe836c8cda0f7e0cba64b559
Haroun wabwire <98280456+haru-voster@users.noreply.github.com>
2023-07-26 17:36:08 +0300

    shell_sort. - knuth sequences

---
+++ 100-shell_sort.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+
+/**
+ * swap - Swap two integers in an array.
+ * @x: The first integer to swap.
+ * @y: The second integer to swap.
+ */
+void swap(int *x, int *y)
+{
+	int tmp;
+
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+
+/**
+ * shell_sort - Sort an array of integers in ascending
+ *              order using the shell sort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Uses the Knuth interval sequence.
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t space, a, b;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (space = 1; space < (size / 3);)
+		space = space * 3 + 1;
+
+	for (; space >= 1; space /= 3)
+	{
+		for (a = space; a < size; a++)
+		{
+			b = a;
+			while (b >= space && array[b - space] > array[b])
+			{
+				swap(array + b, array + (b - space));
+				b -= space;
+			}
+		}
+		print_array(array, size);
+	}
+}
